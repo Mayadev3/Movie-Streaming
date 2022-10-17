5fd6194bd7bc558de40ce96f0504b04801660065
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-17 17:33:58 +0200

    fetched the API url

---
+++ script.js
@@ -0,0 +1,19 @@
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.querySelector(".search");
+
+const apiUrl = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=64b65930978cceae7b4c679e1f043d6f&page=1`;
+console.log(apiUrl);
+const imagePath = `https://image.tmdb.org/t/p/w1280`;
+const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=64b65930978cceae7b4c679e1f043d6f&query="`;
+
+async function getMovies(url) {
+  const get = await fetch(url);
+  const response = await get.json();
+  console.log(response.results);
+}
+getMovies(apiUrl);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const movie = search.value;
+});
